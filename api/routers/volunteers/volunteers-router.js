561626906f2d273395282a43979139dd92a6626b
Gordon Caister <gordoncaister@gmail.com>
2020-05-23 10:13:16 -0500

    added some routing, made server respond to get call

---
+++ api/routers/volunteers/volunteers-router.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const bcryptjs = require("bcryptjs")
+const secrets = require("../../auth/secrets")
+const Volunteers = require("./volunteers-model.js")
+const jwt = require("jsonwebtoken")
+
+function generateToken(user){
+    const payload = {
+        userId : user.id,
+        username : user.username
+    }
+    const secret = secrets.jwtSecret;
+    const options = {
+        expiresIn:"1d"
+    }
+    return jwt.sign(payload, secret, options)
+  }
+  
+  module.exports = router;
