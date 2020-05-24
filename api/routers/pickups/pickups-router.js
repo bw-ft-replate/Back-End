f3ad6365c2015df14ef83fa89dcf29c016265553
Gordon Caister <gordoncaister@gmail.com>
2020-05-24 14:06:40 -0500

    added to pickups router

--- api/routers/pickups/pickups-router.js
+++ api/routers/pickups/pickups-router.js
@@ -3,20 +3,23 @@ const bcryptjs = require("bcryptjs")
 const secrets = require("../../auth/secrets")
 const Pickups = require("./pickups-model.js")
 const jwt = require("jsonwebtoken")
+const moment = require("moment")
 const authenticator = require("../../auth/authenticator")
 
 router.get("/",(req,res)=>{
     Pickups.find().then(allPickups => {
+        
         res.status(200).json(allPickups)
     })
     .catch(err => {
         res.status(500).json(err)
     })
+    console.log(moment("23-03-2020","DD-MM-YYYY").format('ll'))
 })
 
 router.post("/",(req,res)=>{
 
-    const concatPickup = {...req.body, "pickup-date": "2222-02-22"}
+    const concatPickup = {...req.body, "pickup-date": req.body["pickup-date"]}
     Pickups.add(concatPickup).then(newPickup => {
         res.status(200).json(newPickup)
     })
